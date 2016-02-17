35102e7f3af1f832ac9e0b38d30d49cb78df348f
Qiang Chen <cqiang@codeaurora.org>
2016-02-16 22:30:47 -0800

    Revert "MobileNetworkSettings: Only show 4G/3G/2G combined modes"
    This reverts commit 6879230b63b2bd1676d4e3cfaede6889e9870339
    
    CRs-Fixed: 966518
    
    Change-Id: I16f8322e8ee45fed816cf398238d05f2c9396c75

--- src/com/android/phone/MobileNetworkSettings.java
+++ src/com/android/phone/MobileNetworkSettings.java
@@ -779,8 +779,7 @@ public class MobileNetworkSettings extends PreferenceActivity
             // in case it is currently something else. That is possible if user
             // changed the setting while roaming and is now back to home network.
             settingsNetworkMode = preferredNetworkMode;
-        } else if (carrierConfig.getBoolean(CarrierConfigManager.KEY_WORLD_PHONE_BOOL) == true &&
-                getResources().getBoolean(R.bool.world_phone)) {
+        } else if (carrierConfig.getBoolean(CarrierConfigManager.KEY_WORLD_PHONE_BOOL) == true) {
             prefSet.removePreference(mButtonEnabledNetworks);
             // set the listener for the mButtonPreferredNetworkMode list preference so we can issue
             // change Preferred Network Mode.
@@ -838,7 +837,7 @@ public class MobileNetworkSettings extends PreferenceActivity
                     mGsmUmtsOptions = null;
                 }
             } else if (phoneType == PhoneConstants.PHONE_TYPE_GSM) {
-                if (isSupportTdscdma() || !getResources().getBoolean(R.bool.world_phone)) {
+                if (isSupportTdscdma()) {
                     mButtonEnabledNetworks.setEntries(
                             R.array.enabled_networks_tdscdma_choices);
                     mButtonEnabledNetworks.setEntryValues(
@@ -1454,7 +1453,7 @@ public class MobileNetworkSettings extends PreferenceActivity
             case Phone.NT_MODE_TD_SCDMA_WCDMA_LTE:
             case Phone.NT_MODE_TD_SCDMA_LTE_CDMA_EVDO_GSM_WCDMA:
             case Phone.NT_MODE_LTE_CDMA_EVDO_GSM_WCDMA:
-                if (isSupportTdscdma() || !getResources().getBoolean(R.bool.world_phone)) {
+                if (isSupportTdscdma()) {
                     mButtonEnabledNetworks.setValue(
                             Integer.toString(Phone.NT_MODE_TD_SCDMA_LTE_CDMA_EVDO_GSM_WCDMA));
                     mButtonEnabledNetworks.setSummary(R.string.network_lte);
